06ca86ec245d4b5039dbd122c6355b4f3ff40b66
Equwece <99554597+Equwece@users.noreply.github.com>
2025-07-14 15:41:47 +0400

    Serialize enum case via MirroredLabel

--- modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggable.scala
+++ modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggable.scala
@@ -5,12 +5,15 @@ import scala.deriving.Mirror
 trait SingletonEnumLoggable[A] extends SingleValueLoggable[A]
 
 object SingletonEnumLoggable:
-  def makeInstance[T]: SingletonEnumLoggable[T] =
+  inline def nameOf[A](using m: scala.deriving.Mirror.Of[A]): String =
+   scala.compiletime.constValue[m.MirroredLabel]
+
+  inline def makeInstance[T: Mirror.Of]: SingletonEnumLoggable[T] =
     new SingletonEnumLoggable[T]:
-      def logValue(a: T): LogParamValue = StrValue(a.toString)
+      def logValue(a: T): LogParamValue = StrValue(nameOf[T])
 
       override def putField[I, V, R, M](a: T, name: String, input: I)(implicit receiver: LogRenderer[I, V, R, M]): R =
-        receiver.addString(name, a.toString, input)
+        receiver.addString(name, nameOf[T], input)
 
   inline def derived[T: Mirror.SumOf]: SingletonEnumLoggable[T] =
     SingletonEnumLoggableMacro.ensureSingletonEnum[T]
